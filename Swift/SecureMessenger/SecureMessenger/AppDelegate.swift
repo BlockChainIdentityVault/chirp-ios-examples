4679f60fc3db5e13ed0d40571fbd842733a93246
Joe Todd <joe@chirp.io>
2019-04-26 17:17:59 +0100

    Add SecureMessenger example

---
+++ Swift/SecureMessenger/SecureMessenger/AppDelegate.swift
@@ -0,0 +1,86 @@
+/*------------------------------------------------------------------------------
+ *
+ *  AppDelegate.swift
+ *
+ *  For full information on usage and licensing, see https://chirp.io/
+ *
+ *  Copyright © 2011-2019, Asio Ltd.
+ *  All rights reserved.
+ *
+ *----------------------------------------------------------------------------*/
+
+import UIKit
+import ChirpConnect
+
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    var sdk: ChirpConnect?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        sdk = ChirpConnect(appKey: CHIRP_APP_KEY, andSecret: CHIRP_APP_SECRET)
+        if let sdk = sdk {
+            if let error = sdk.setConfig(CHIRP_APP_CONFIG) {
+                print(error.localizedDescription)
+            }
+            if let error = sdk.start() {
+                print(error.localizedDescription)
+            }
+        }
+        
+        return true
+    }
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        if let sdk = sdk {
+            if sdk.state != CHIRP_CONNECT_STATE_STOPPED {
+                if let error = sdk.stop() {
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        if let sdk = sdk {
+            if sdk.state != CHIRP_CONNECT_STATE_STOPPED {
+                if let error = sdk.stop() {
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        if let sdk = sdk {
+            if sdk.state == CHIRP_CONNECT_STATE_STOPPED {
+                if let error = sdk.start() {
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        if let sdk = sdk {
+            if sdk.state == CHIRP_CONNECT_STATE_STOPPED {
+                if let error = sdk.start() {
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        if let sdk = sdk {
+            if sdk.state != CHIRP_CONNECT_STATE_STOPPED {
+                if let error = sdk.stop() {
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+}
